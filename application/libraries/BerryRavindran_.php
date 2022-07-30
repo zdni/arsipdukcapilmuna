55addb3378fd50f27141bc9f0e41afed3c288e92
piyo02 <alzidni2000@gmail.com>
2022-07-30 13:13:19 +0800

    bug algoritma

--- application/libraries/BerryRavindran_.php
+++ application/libraries/BerryRavindran_.php
@@ -10,6 +10,7 @@ class BerryRavindran
     public function search( $keyword, $filter, $datas )
     {
         $results = [];
+        // die;
         
         // pre processing
         $keyword = strtolower( $keyword );
@@ -18,28 +19,32 @@ class BerryRavindran
         $key_array_sort = $key_array;
         sort($key_array_sort);
         $key_array_sort[] = '*';
-        $key_array_sort[] = '*';
+        $key_array[] = '*';
         
         $tabel_BRbc_shift = [];
 
-        foreach ($key_array_sort as $key) {
+        foreach ($key_array_sort as $key => $value) {
             $tabel_BRbc_shift[$key] = [];
             foreach ($key_array_sort as $key_) {
-                $tabel_BRbc_shift[$key][$key_] = $key_length+2;
+                $tabel_BRbc_shift[$key][] = $key_length+2;
             }
         }
         foreach ($tabel_BRbc_shift as $key => $value) {
-            $tabel_BRbc_shift[$key][$key_array[0]] = $key_length+1;
+            $index = array_search( $key_array[0], $key_array_sort );
+            $tabel_BRbc_shift[$key][$index] = $key_length+1;
         }
         for ($i=0; $i < $key_length; $i++) { 
+            $index = array_search( $key_array[$i], $key_array_sort );
+            $index_plus_1 = array_search( $key_array[$i+1], $key_array_sort );
             if( $i == ($key_length-1)) {
-                $tabel_BRbc_shift[$key_array[$i]]['*'] = $key_length-($i);
+                $tabel_BRbc_shift[$index][$key_length] = $key_length-($i);
             } else {
-                $tabel_BRbc_shift[$key_array[$i]][$key_array[$i+1]] = $key_length-($i);
+                $tabel_BRbc_shift[$index][$index_plus_1] = $key_length-($i);
             }
         }
         foreach ($tabel_BRbc_shift as $key => $value) {
-            $tabel_BRbc_shift[$key_array[$key_length-1]][$key] = 1;
+            $index = array_search( $key_array[$key_length-1], $key_array_sort );
+            $tabel_BRbc_shift[$index][$key] = 1;
         }
 
         // fase pencarian
@@ -57,7 +62,7 @@ class BerryRavindran
                     $results[] = $data;
                     break;
                 } else {
-                    if( $end+$start >= $kata_length || $end+$start+1 >= $kata_length ) break;
+                    if( $end+$start > $kata_length || $end+$start+1 > $kata_length ) break;
     
                     $a = in_array( $kata_array[$end+$start], $key_array ) ? $kata_array[$end+$start] : '*';
                     $b = in_array( $kata_array[$end+$start+1], $key_array ) ? $kata_array[$end+$start+1] : '*';
